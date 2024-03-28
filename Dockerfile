e14431a624e7c2e5a0219467b81c7d53af3e8c3f
Joseph P <s3ppzer0@gmail.com>
2024-03-28 18:09:33 +0000

    refactor: update variable usage, PHP and Nginx versions

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-# DoD: Installing Nginx with Docker
-FROM nginx:1.25.2-bookworm
+# step: installing Nginx with Docker
+FROM nginx:1.25.4-bookworm
 EXPOSE 80
 EXPOSE 22
 
-# DoD: Deploying DVWA on Nginx
+WORKDIR /ndvwa
+
+# step: deploying DVWA on Nginx
 # copy config files into container
 COPY configs/dvwa.conf /etc/nginx/sites-available/default
 COPY configs/nginx.conf /etc/nginx/nginx.conf
-COPY configs/dbsetup.sql /dbsetup.sql
+COPY configs/dbsetup.sql ${WORKDIR}/dbsetup.sql
+COPY entrypoint.sh /ndvwa/entrypoint.sh
 # install basic packages
 RUN apt-get update && \
     apt-get install -y \
@@ -25,29 +28,27 @@ RUN apt-get update && \
         openssh-server \
         openssh-client \
         sshpass \
-        knockd
+        knockd && \
+    apt-get autoremove -y
 # install a specific version of PHP
 RUN wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg && \
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list && \
-    apt update && \
-    apt -y install php7.4-fpm php7.4-mysqli
+    apt-get update && \
+    apt-get install -y php8.3-fpm php8.3-mysqli
 # run configurations
-RUN mkdir -p /etc/nginx/sites-available && \
-    mkdir -p /etc/nginx/sites-enabled && \
-    mkdir -p /var/www/html && \
+RUN mkdir -p /etc/nginx/sites-available /etc/nginx/sites-enabled /var/www/html && \
     # prepare DVWA files
     git clone --depth 1 https://github.com/digininja/DVWA.git /var/www/html/dvwa && \
     chmod 777 -R /var/www/html/dvwa && \
     ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default && \
     # create PHP config
-    cd /var/www/html/dvwa && \
-    cp config/config.inc.php.dist config/config.inc.php && \
+    cp /var/www/html/dvwa/config/config.inc.php.dist /var/www/html/dvwa/config/config.inc.php && \
     # setup database
     service mariadb start && \
-    mysql -u root < /dbsetup.sql && \
+    mysql -u root < ${WORKDIR}/dbsetup.sql && \
     # firewall setting for nginx
     ufw allow 80,443/tcp
 
-# setup entrypoint (technically CMD)
-COPY entrypoint.sh /entrypoint.sh
-CMD [ "bash", "/entrypoint.sh" ]
+# setup entrypoint CMD
+COPY entrypoint.sh ${WORKDIR}/entrypoint.sh
+CMD [ "bash", "/ndvwa/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
 
+IP="172.17.0.1"
+
+
 # start services
 service mariadb start
-service php7.4-fpm start
+service php8.3-fpm start
 service nginx start
 sleep 1
 
 # prompt to either proceed with SSH configurations or jump into shell
 printf "\n\n"
 read -p "[ ? ] Proceed with SSH configurations? [yes/no] " yn
-case $yn in 
-  yes ) 
-      # DoD: Establishing Reverse Tunneling to SSH
+case $yn in
+  yes )
+      # step: establishing reverse tunneling to SSH
       printf "\n\n== Establishing Reverse Tunneling to SSH ==\n"
       # ask for host's credentials, which will be required for sudo operations
       printf "\n[ * ] Please enter the following information from you host environment.\n"
       read -p "    - Username: " USER
       read -s -p "    - Password: " PASS
       printf "\n"
-      # setup containers SSH keys and connection to the SSH server
+      # setup container's SSH keys and connection to the SSH server (host environment)
       printf "\n[ * ] Setting up SSH keys.\n\n"
       ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
-      sshpass -p $PASS ssh-copy-id -o StrictHostKeyChecking=no $USER@172.17.0.1
+      sshpass -p ${PASS} ssh-copy-id -o StrictHostKeyChecking=no ${USER}@${IP}
       printf "\n[ * ] Configuring reverse tunneling.\n\n"
-      ssh -f -N -R 2222:localhost:22 $USER@172.17.0.1
+      ssh -f -N -R 2222:localhost:22 ${USER}@${IP}
       printf "[ * ] Opening SSH connection. When ready, \"exit\" it to proceed with port knocking protection setup.\n\n"
-      ssh -p 22 $USER@172.17.0.1
+      ssh -p 22 ${USER}@${IP}
 
-      # DoD: Protecting SSH from Nmap Scanning (port knocking protection)
+      # step: protecting SSH from Nmap scanning with port knocking protection
       printf "\n\n== Protecting SSH from Nmap Scanning ==\n"
       printf "\n[ * ] Please enter the following information from you host environment.\n"
       read -p "    - Full path to this repository: " RPATH
       printf "\n"
       printf "\n[ * ] Checking that SSH port is currently open using NMAP.\n\n"
-      nmap -p 22 172.17.0.1
+      nmap -p 22 ${IP}
       printf "\n[ * ] Configuring knockd service on the host machine (via SSH).\n\n"
-      ssh -p 22 $USER@172.17.0.1 -t "cd $RPATH && echo $PASS | sudo -S bash knockd_setup.sh"
+      ssh -p 22 ${USER}@${IP} -t "cd ${RPATH} && echo ${PASS} | sudo -S bash knockd_setup.sh"
       sleep 1
       printf "\n[ * ] Attemting to scan the SSH port and connect to the SSH server with knockd service running.\n\n"
-      nmap -p 22 172.17.0.1
-      ssh -p 22 $USER@172.17.0.1
+      nmap -p 22 ${IP}
+      ssh -p 22 ${USER}@${IP}
       printf "\n[ * ] Executing magic knock-knock sequence and actually connecting to the SSH server. When ready, \"exit\" it to proceed.\n\n"
-      knock -v 172.17.0.1 20001 20002 20003 -d 500
-      ssh -p 22 $USER@172.17.0.1
+      knock -v ${IP} 20001 20002 20003 -d 500
+      ssh -p 22 ${USER}@${IP}
       printf "\n[ * ] Restoring iptables rules on the host machine.\n\n"
-      knock -v 172.17.0.1 20001 20002 20003 -d 500
-      ssh -p 22 $USER@172.17.0.1 -t "echo $PASS | sudo -S iptables -F DOCKER && sudo -S iptables -F INPUT && sudo -S service knockd stop"
+      knock -v ${IP} 20001 20002 20003 -d 500
+      ssh -p 22 ${USER}@${IP} -t "echo ${PASS} | sudo -S iptables -F DOCKER && sudo -S iptables -F INPUT && sudo -S service knockd stop"
       ;;
   no )
      printf "[ * ] Jumping directly into Bash shell..\n\n";;
